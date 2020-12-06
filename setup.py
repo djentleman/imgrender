582a05a4b3dc6de8de9feae5816a9e8b210fc821
Todd Perry <up612136@myport.ac.uk>
2020-12-06 20:23:47 +0900

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="imgrender",
-    version="0.0.2",
+    version="0.0.3",
     author="Todd Perry",
     author_email="todd.perry@myport.ac.uk",
     description="Python terminal image renderer",
